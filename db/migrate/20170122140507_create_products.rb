820ee9aac45d93e06dc8b8e49c05ae5376b3fb3a
benoit nguyen <nguyen.hl.b@gmail.com>
2017-01-22 15:10:32 +0100

    add model controller products

---
+++ db/migrate/20170122140507_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :first_name
+      t.string :last_name
+      t.text :description
+      t.float :value
+      t.boolean :for_sale
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
