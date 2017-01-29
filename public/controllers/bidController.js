cd3b11174d40e47c1529ce1320255d020e378e66
benoit nguyen <nguyen.hl.b@gmail.com>
2017-01-29 19:23:43 +0100

    fixed date problem in bid form

--- public/controllers/bidController.js
+++ public/controllers/bidController.js
@@ -1,36 +1,18 @@
 app.controller('bidCtrl', [ '$scope', 'authService', 'dataService', '$state', '$location', function($scope, authService, dataService, $state, $location) {
 
   var end_auto_date = new Date();
+  var date = new Date();
+  $scope.date = date
   $scope.end_auto_date = end_auto_date;
   $scope.month = ['Januar', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   $scope.monthShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   $scope.weekdaysFull = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   $scope.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-  $scope.disable = [false, 1, 7];
   $scope.today = 'Today';
   $scope.clear = 'Clear';
   $scope.close = 'Close';
   var days = 15;
-  $scope.minDate = (new Date($scope.end_auto_date.getTime() - ( 1000 * 60 * 60 *24 * days ))).toISOString();
-  $scope.maxDate = (new Date($scope.end_auto_date.getTime() + ( 1000 * 60 * 60 *24 * days ))).toISOString();
-  $scope.onStart = function () {
-      console.log('onStart');
-  };
-  $scope.onRender = function () {
-      console.log('onRender');
-  };
-  $scope.onOpen = function () {
-      console.log('onOpen');
-  };
-  $scope.onClose = function () {
-      console.log('onClose');
-  };
-  $scope.onSet = function () {
-      console.log('onSet');
-  };
-  $scope.onStop = function () {
-      console.log('onStop');
-  };
+  $scope.minDate = (new Date($scope.date)).toISOString();
 
   $scope.bid = {
 
